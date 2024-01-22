f6b5ed9ec3e59437e6fe90470514c31973499e40
Ruben <camac@LAPTOP-GQBG0BGB>
2024-01-22 18:26:04 +0100

    Se refactoriza el ejercicio 3

--- ejercicioRefactoring/src/ejerciciosBase/ejercicio3.java
+++ ejercicioRefactoring/src/ejerciciosBase/ejercicio3.java
@@ -1,45 +1,51 @@
 package ejerciciosBase;
-
 import java.util.Scanner;
-
 public class ejercicio3 {
-	
-	/*
-	 * Escribe un programa que muestre por pantalla todos los números enteros positivos
-		menores a uno leído por teclado que no sean divisibles entre otro también leído de
-		igual forma
-	 * */
 
 	public static void main(String[] args) {
-			
-		introducirNumero_Divisor();
+
+		int numero=IntroNum();
+		int divisor=IntroDiv();
+		MostrarLista(numero,divisor);
+
 	}
-	
-	public static void introducirNumero_Divisor() {
-		
-		Scanner sc = new Scanner(System.in);	
-		
-		int numero, divisor;
-		
-		System.out.println("Introduce un número: ");
+	public static int IntroNum() {
+		int numero;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduce un número:");
 		numero = sc.nextInt();
 		
-		System.out.println("Introduce el divisor: ");		
-		divisor = sc.nextInt();
 		
-		comprobador(numero , divisor);
+		return numero;
 		
-		sc.close();
 	}
-	
-	
-	public static void comprobador(int numero , int divisor) {
+	public static int IntroDiv() {
+		Scanner sc = new Scanner(System.in);
+		int divisor;
+		System.out.println("Introduce el divisor:");
+		do {
+			divisor = sc.nextInt();
+			if(divisor==0) {
+				System.out.println("Error al introducir divisor,introduzca otro");
+			}
+		}while(divisor==0);
 		
-		for (int i = 1; i <= numero; i++) { 
-			
+		return divisor;
+		
+		
+	}
+	public static void MostrarLista(int numero,int divisor) {
+		
+		for (int i = 1; i <= numero; i++) {
+			// de ese listado de números comprueba cuales NO son divisbles entre la variable
+			// divisor
 			if (i % divisor != 0) {
+				// solo printea en caso de que no sean divisibles
 				System.out.println(i);
 			}
 		}
 	}
+
 }
